a86bef274518cecc10d1cb4f7d389b2c0bdfb323
qaisermajeed007 <qaiser_majeed@live.com>
2022-07-23 21:54:22 +0100

    added firebase support

--- src/app/scripts/App.js
+++ src/app/scripts/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 const App = () => {
   return (
     <div class="geotabPageHeader">
       <h1 class="geotabPageName">
-        Page Name <span class="subdued">with sub title</span>
+        Hafiz Qaiser<span class="subdued">Add working with React JS</span>
       </h1>
       <div class="pageNameSecondary">
         <h3 class="geotabSecondaryTitle">and additional information</h3>
       </div>
+      <Example />
     </div>
   );
 };
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
 
 export default App;
